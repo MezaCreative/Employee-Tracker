d973053ab189966c804be1816a51e379becf69ce
Esteban Meza <meza.19@osu.edu>
2021-03-24 01:29:17 -0400

    fixed typos in schema that gave MYSQL a hard time

--- schema.sql
+++ schema.sql
@@ -20,13 +20,13 @@ CREATE TABLE role (
 );
 
 CREATE TABLE employee (
-    id INT NOTNULL AUTO_INCREMENT,
+    id INT NOT NULL AUTO_INCREMENT,
     title VARCHAR(30),
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    role_id INT NOT NULL
+    role_id INT NOT NULL,
     manager_id INT,
-    FOREIGN KEY(role_id) REFERENCES role(id)
-    FOREIGN KEY (manager_id) REFERENCES manager(id)
+    FOREIGN KEY(role_id) REFERENCES role(id),
+    FOREIGN KEY (manager_id) REFERENCES employee(id),
     PRIMARY KEY(id)
-)
+);
