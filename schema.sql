a1d1bcd8c14296a1da862682f7a08a7c731b6006
Esteban Meza <meza.19@osu.edu>
2021-03-24 01:20:21 -0400

    added the works to server.js and schema.sql

--- schema.sql
+++ schema.sql
@@ -1,1 +1,32 @@
-DROP DATABASE IF EXISTS employees_DB
+DROP DATABASE IF EXISTS employees_DB;
+
+CREATE DATABASE employees_DB;
+
+USE employees_DB;
+
+CREATE TABLE department (
+    id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(30) NOT NULL,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE role (
+    id INT AUTO_INCREMENT,
+    title VARCHAR(30) NOT NULL,
+    salary DECIMAL(10, 2) NOT NULL,
+    department_id INT,
+    FOREIGN KEY (department_id) REFERENCES department(id),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE employee (
+    id INT NOTNULL AUTO_INCREMENT,
+    title VARCHAR(30),
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    role_id INT NOT NULL
+    manager_id INT,
+    FOREIGN KEY(role_id) REFERENCES role(id)
+    FOREIGN KEY (manager_id) REFERENCES manager(id)
+    PRIMARY KEY(id)
+)
---
+++ server.js
@@ -0,0 +1,17 @@
+const mysql = require ("mysql");
+
+const connection = mysql.createConnection({
+    host: "localhost",
+
+    port:3306,
+
+    user: "root",
+
+    database: "employess_db",
+});
+
+connection.connect((err) => {
+    if (err) throw err;
+    console.log('connected as id ' + connection.threadId);
+    connection.end();
+});
