b77dada4d65964da749104034f7f78b4ca4158dd
Esteban Meza <meza.19@osu.edu>
2021-03-26 02:03:32 -0400

    Added additional questions

--- server.js
+++ server.js
@@ -1,3 +1,6 @@
+// Esteban Meza
+// @MezaCreative
+//NOTE: Not many comments these functions are self-explanatory
 const inquirer = require("inquirer");
 const mysql = require ("mysql");
 
@@ -36,9 +39,11 @@ function startPrompt() {
                 break;
 
             case "View a department, role, or employee":
+                view();
                 break;
 
             case "Update employee role":
+                update();
                 break;
 
             case "End":
@@ -63,10 +68,10 @@ function add() {
                 addDepartment();
                 break;
             case "Role":
-
+                addRole();
                 break;
             case "Employee":
-
+                addEmployee();
                 break;  
         }
     })
@@ -88,10 +93,181 @@ function addDepartment() {
                 },
                 function(err, res) {
                     if (err) throw err;
-                    console.log(res.affectedRows +  "added\n");
+                    console.log(res.affectedRows +  "department(s) added\n");
                     startPrompt();
                 }
             );
         })
     
+}
+function addRole() {
+    inquirer.prompt([
+        {
+            name: "roleName",
+            type:"input",
+            message: "Enter the name of the role."
+        },
+        {
+            name: "roleSalary",
+            type:"input",
+            message: "What is the salary for such role?"
+        },
+        {
+        name: "roleDepartment",
+        type: "input",
+        message: "What is the roles department?"
+         }
+    ])
+        .then((answer) => {
+            console.log("Inserting role....\n......\n.......\n");
+            connection.query(
+                "INSERT INTO role set ?",
+                {
+                    title: answer.roleName,
+                    salary: answer.roleSalary,
+                    department_id: answer.roleDepartment
+                },
+                function (err, res) {
+                    if (err) throw err;
+                    console.log(res.affectedRows +  " role(s) added!\n");
+                    startPrompt();
+                }
+            )
+        })
+}
+
+function addEmployee(){
+    inquirer.prompt([
+        {
+        name: "employeeFirstName",
+        type: "input",
+        message: "Enter the Employee's first name?"
+        },
+        {
+            name: "employeeLastName",
+            type: "input",
+            message: "Enter the Employee's last name?"
+        },
+        {
+            name: "employeeId",
+            type: "input",
+            message: "Enter the Employee's role Id?"
+        },
+        {
+            name: "employeeMan",
+            type: "input",
+            message: "Enter the Employee's Manager Id?"
+        },
+    ])
+        .then((answer) => {
+            console.log("Insterting Employee info ......\n .....\n.......\n");
+            connection.query(
+                "INSERT INTO employee SET ?",
+                {
+                    first_name: answer.employeeFirstName,
+                    last_name: answer.employeeLastName,
+                    role_id: answer.employeeId,
+                    manager_id: answer.employeeMan
+                },
+                function(err, res) {
+                    if (err) throw err;
+                    console.log(res.affectedRows +  " employee(s) added!\n");
+                    startPrompt();
+                }
+            );
+        })
+}
+
+function view() {
+    inquirer.prompt({
+        name: "viewType",
+        type:"list",
+        message: "Which would you like to view?",
+        choices: ["Department", "Role", "Employee"]
+    })
+    .then((answer) => {
+        switch (answer.viewType) {
+            case "Department":
+                console.log("before");
+                viewDepartment();
+                break;
+            case "Role":
+                viewRole();
+                break;
+            case "Employee":
+                viewEmployee();
+                break;  
+        }
+    })
+}
+
+function viewDepartment() {
+        connection.query("SELECT * FROM department;",
+        function (err, res) {
+            if (err) throw err;
+            console.table(res);
+            startPrompt();
+        }
+        )
+}
+
+function viewRole(){
+    connection.query("SELECT * FROM role;",
+    function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startPrompt();
+    }
+    )
+}
+function viewEmployee() {
+    connection.query("SELECT * FROM employee;",
+    function (err, res) {
+        if (err) throw err;
+        console.table(res);
+        startPrompt();
+    }
+    )
+}
+
+function update(){
+    inquirer.prompt({
+        name: "updatePerson",
+        type: "intput",
+        message: "Please enter the Id number for the person being updated."
+    },
+        {
+        name: "updateType",
+        type:"list",
+        message: "What would you like to update?",
+        choices: ["Employee Role", "Employee Manager"]
+    },
+    )
+    .then((answer) => {
+        
+        switch (answer.updateType) {
+            case "Employee Role":
+                const newRole = findID(answer.updateType);
+                console.log( "Employee Role is " + newRole);
+                startPrompt();
+                break;
+            case "Employee Manager":
+                const newMan = findID(answer.updateType);
+                console.log("Employee Manager is " + newMan);
+                startPrompt();
+                break;
+        }
+    })
+    
+}
+
+function findID(type) {
+    inquirer.prompt({
+        name: "idNum",
+        type: "input",
+        message: "Please enter the " + type + " Id Number."
+    })
+    .then((answer) => {
+        return answer.idNum;
+    })
 }
