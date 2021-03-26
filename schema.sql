5b7e969c1dc14dccb8c5405bf7207d4f02d553ae
Esteban Meza <meza.19@osu.edu>
2021-03-26 02:04:40 -0400

    fixed the errors on this

--- schema.sql
+++ schema.sql
@@ -4,6 +4,7 @@ CREATE DATABASE employees_DB;
 
 USE employees_DB;
 
+
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT,
     name VARCHAR(30) NOT NULL,
@@ -19,13 +20,14 @@ CREATE TABLE role (
     PRIMARY KEY (id)
 );
 
+
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
     role_id INT NOT NULL,
     manager_id INT,
-    FOREIGN KEY(role_id) REFERENCES role(id),
-    FOREIGN KEY (manager_id) REFERENCES employee(id),
+	FOREIGN KEY (manager_id) REFERENCES employee (id),
+	FOREIGN KEY (role_id) REFERENCES role(id),
     PRIMARY KEY(id)
 );
